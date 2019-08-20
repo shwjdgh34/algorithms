af09c6a644c4274a7f9b17f5bb4a86ca688d6f77
Jeongho Noh <nossi8128@gmail.com>
2019-08-20 10:58:26 +0900

    almost done. but have a problem. when find(), itr dont direct head but NULL.

--- Problems/dirManager/dirManager.cpp
+++ Problems/dirManager/dirManager.cpp
@@ -34,10 +34,12 @@ int find(char keyword[]);
 
 int curDir;
 int dirTotalNum;
+int dirIdx;
 char upper[MAX_LEN] = UPPER;
 char root[MAX_LEN] = ROOT;
 char star[MAX_LEN] = STAR;
 int removeCount;
+int foundCount;
 typedef struct _node
 {
     char name[MAX_LEN];
@@ -52,19 +54,17 @@ public:
     int idx;
     int childNum; // 자식 dir의 개수
     Node *head;
-    Node *tail;
     Node *itr;
     Linkedlist()
     {
         head = NULL;
-        tail = NULL;
         itr = NULL;
         childNum = 0;
     }
     void add(char keyword[]);
 
     // keyword와 같은 폴더명이 있는지 찾고, 찾았다면 curDir를 해당 idx로 바꿔주고 true 반환
-    bool find(char keyword[])
+    bool seek(char keyword[])
     {
         while (itr != NULL)
         {
@@ -77,78 +77,139 @@ public:
         }
         return false;
     }
+    void LLfind(char keyword[]);
     void change(char keyword[])
     {
         if (strcmp(keyword, upper))
             curDir = parent;
         else if (strcmp(keyword, root))
             curDir = 0;
-        else if (find(keyword))
+        else if (seek(keyword))
         {
             curDir = itr->idx;
             itr = head;
         }
     }
     void removeNodes(char keyword[]);
+    void clear()
+    {
+        childNum = 0;
+        parent = -1;
+        itr = head;
+        while (itr != NULL)
+        {
+            Node *tmpDel = itr;
+            itr->idx = 0;
+            itr->next = NULL;
+            itr->prev = NULL;
+            itr = itr->next;
+            delete tmpDel;
+        }
+        head = NULL;
+    }
 };
 Linkedlist fileList[MAX_NUM];
 
 void Linkedlist::add(char keyword[])
 {
-    if (find(keyword)) // 해당 dir이 있다면 바로 return
+    if (seek(keyword))
+    { // 해당 dir이 있다면 바로 return
+        itr = head;
         return;
+    }
     else // 해당 이름의 dir이 없다면 만들어준다.
     {
         Node *newNode = new Node;
         strcpy(newNode->name, keyword);
         newNode->next = NULL;
         newNode->prev = NULL;
-        newNode->idx = dirTotalNum++;
+        newNode->idx = dirIdx++;
+        dirTotalNum++;
         fileList[newNode->idx].parent = idx;       // parent's idx
         fileList[newNode->idx].idx = newNode->idx; // child'x idx;
 
         if (head == NULL) // 처음 만들어 주는 경우
         {
             head = newNode;
-            tail = newNode;
             itr = head;
         }
         else
         {
-            tail->next = newNode;
-            newNode->prev = tail;
-            tail = newNode;
+            head->prev = newNode;
+            newNode->next = head;
+            head = newNode;
+            itr = head;
         }
         childNum++;
     }
 }
 void Linkedlist::removeNodes(char keyword[])
 {
+    /*
+    // 폴더 안에 아무 내용도 없을 경우
+    if (fileList[curDir].head == NULL)
+        return;
+    // basecase
     if (head == NULL)
     {
-        removeCount++;
         return;
     }
-    while (find(keyword))
+    */
+    while (seek(keyword))
     {
         Node *delTmp = itr;
 
         fileList[itr->idx].removeNodes(star);
-        if (itr != head && itr != tail)
+        if (itr != head && itr->next != NULL) // 중간에 있는 노드들
         {
             Node *tmpPrev = itr->prev;
             Node *tmpNext = itr->next;
             tmpPrev->next = tmpNext;
             tmpNext->prev = tmpPrev;
             itr = tmpNext;
-            delete delTmp;
-            childNum--;
         }
         else if (itr == head)
         {
-            head =
+            if (itr->next == NULL)
+            { // data가 하나일때
+                itr = NULL;
+                head = NULL;
+            }
+            else
+            { //data가 여러개 있을 때
+                head = itr->next;
+                head->prev = NULL;
+                itr = head;
+            }
         }
+        else if (itr->next == NULL) // 맨 마지막일 때
+        {
+            Node *tmpPrev = itr->prev;
+            tmpPrev->next = NULL;
+            itr = NULL;
+        }
+        dirTotalNum--;
+        childNum--;
+        removeCount++;
+        delete delTmp;
     }
+    itr = head;
+}
+void Linkedlist::LLfind(char keyword[])
+{
+    while (seek(keyword))
+    {
+        foundCount++;
+        itr = itr->next;
+    }
+
+    itr = head;
+    while (itr != NULL)
+    {
+        fileList[itr->idx].LLfind(keyword);
+        itr = itr->next;
+    }
+    itr = head;
 }
 //-----------------------------------------------
 static int run(int Ans)
@@ -217,7 +278,12 @@ void init()
 {
     //--------------------------------
     //  코드 작성
+    for (int i = 0; i <= dirIdx; i++)
+    {
+        fileList[i].clear();
+    }
     dirTotalNum = 1;
+    dirIdx = 1;
     curDir = 0; // root 폴더
     fileList[0].idx = 0;
     fileList[0].parent = -1; // 최상위라 parent가 없다.
@@ -251,8 +317,10 @@ int find(char keyword[])
 {
     //--------------------------------
     //  코드 작성
+    foundCount = 0;
+    fileList[curDir].LLfind(keyword);
+    return foundCount;
     //--------------------------------
-    return 0;
 }
 void strcpy(char *target, char *source)
 {
@@ -269,11 +337,13 @@ bool strcmp(char *target, char *source)
     int i = 0;
     while (source[i] != '\0')
     {
+
         if (source[i] == '*')
             return true;
+        if (target[i] == '\0')
+            return false;
         if (source[i] != target[i])
             return false;
-
         i++;
     }
     if (target[i] != '\0')
