e3c594e25969094d6a83de16e18f702535fcde06
Jeongho Noh <nossi8128@gmail.com>
2019-08-27 20:13:37 +0900

    declard glbalN, so reduced time to 1/10

--- Problems/targetNumber/targetNumber.cpp
+++ Problems/targetNumber/targetNumber.cpp
@@ -3,21 +3,23 @@
 
 using namespace std;
 
-void dfs(vector<int> numbers, int target, int idx, int sum, int *pAnswer)
+vector<int> globalN;
+void dfs(int target, int idx, int sum, int *pAnswer)
 {
-    if (numbers.size() == idx)
+    if (globalN.size() == idx)
     {
         if (sum == target)
             *pAnswer += 1;
         return;
     }
-    dfs(numbers, target, idx + 1, sum + numbers[idx], pAnswer);
-    dfs(numbers, target, idx + 1, sum - numbers[idx], pAnswer);
+    dfs(target, idx + 1, sum + globalN[idx], pAnswer);
+    dfs(target, idx + 1, sum - globalN[idx], pAnswer);
 }
 int solution(vector<int> numbers, int target)
 {
     int answer = 0;
-    dfs(numbers, target, 0, 0, &answer);
+    globalN = numbers;
+    dfs(target, 0, 0, &answer);
 
     return answer;
 }
