0982dd79f189500bc9a26bbece6cdad608a0c230
Jeongho Noh <nossi8128@gmail.com>
2019-09-18 00:43:12 +0900

    first commit

---
+++ kakao2017/secretMap/secretMap.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<string> solution(int n, vector<int> arr1, vector<int> arr2)
+{
+    vector<string> answer(n);
+    int arrSize = arr1.size();
+    vector<int> arrAnswer(arrSize);
+    for (int i = 0; i < arrSize; i++)
+    {
+        arrAnswer[i] = arr1[i] | arr2[i];
+    }
+    int j = 1 << (n - 1);
+    for (int i = 0; i < n; i++)
+    {
+        for (int k = 0; k < n; k++)
+        {
+            if (arrAnswer[k] & j)
+                answer[k].push_back('#');
+            else
+                answer[k].push_back(' ');
+        }
+        j = j >> 1;
+    }
+    return answer;
+}
+int main()
+{
+    int n = 5;
+    vector<int> arr1{9, 20, 28, 18, 11};
+    vector<int> arr2{30, 1, 21, 17, 28};
+    vector<string> answer = solution(n, arr1, arr2);
+    for (int i = 0; i < n; i++)
+    {
+        cout << answer[i] << endl;
+    }
+}
